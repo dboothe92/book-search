8c692e31ed0ed1f7b3035e82368337fc3c677873
Dan B <dbraunby@hotmail.com>
2021-06-17 18:02:09 -0600

    removed uneeded routes directory and requirements in server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 const db = require('./config/connection');
-//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -29,8 +28,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
